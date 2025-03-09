c728fefc09671072fb27dd70ae4694096f12829c
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-03-09 22:35:00 +0530

    OTP verification field was added

---
+++ Sora/Sora/View/Authentication/VerificationView.swift
@@ -0,0 +1,18 @@
+//
+//  otpView.swift
+//  Sora
+//
+//  Created by Manik Lakhanpal on 09/03/25.
+//
+
+import SwiftUI
+
+struct otpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    otpView()
+}
